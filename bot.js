6ac4417074dad0f3ed83d4937bf7c9b540a1d5bc
Dxx77 <69280645+Dxx77@users.noreply.github.com>
2020-08-06 01:46:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Supa`,"http://twitch.tv/the19xx")
+client.user.setGame(`77x`,"http://twitch.tv/diaax_77")
 client.user.setStatus("dnd")
 });
 
